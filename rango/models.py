d96a7d957670072ef2e856635877cc0a1d14d95a
2598798g@student.gla.ac.uk <2598798g@student.gla.ac.uk>
2021-07-30 14:22:05 +0800

    Chapter 5 completed

--- rango/models.py
+++ rango/models.py
@@ -4,11 +4,11 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    
-    class Meta:
-         verbose_name_plural = 'Categories'
 
-    def __str__(self): 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
@@ -16,6 +16,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
-def __str__(self): 
-    return self.title
+
+    def __str__(self):
+        return self.title
