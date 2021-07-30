a01f221b6685c2bf685e6316bed5af9be628768d
2598798g@student.gla.ac.uk <2598798g@student.gla.ac.uk>
2021-07-30 16:54:31 +0800

    no message

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from rango.models import Page, Category
-
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
